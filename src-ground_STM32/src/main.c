1948c6f4864c7ec7f0f961ce527eba61acf6c4e6
Korr237i <qwerty2012i@yandex.ru>
2017-06-13 22:26:25 +0300

    Настроили наземку на вывод ЧБ изображений в формате YCrCb. Цветные пока не получаются

--- src-ground_STM32/src/main.c
+++ src-ground_STM32/src/main.c
@@ -12,18 +12,22 @@
 #include <string.h>
 #include <math.h>
 
-#define START_FRAME		0xFF7C0000
-#define END_ROW			0xFF7C00FF
-#define END_FRAME		0x007CFFFF
+#define INV_START_FRAME		0xFF7C0000
+#define INV_END_ROW			0xFF7C00FF
+#define INV_END_FRAME			0x007CFFFF
 
-#define INV_START_FRAME	0x0000FF7C
-#define INV_END_ROW		0x00FFFF7C
-#define INV_END_FRAME	0xFFFF007C
+#define START_FRAME			0x0000FF7C
+#define END_ROW				0x00FFFF7C
+#define END_FRAME				0xFFFF007C
 
 #define MARKER_SIZE		4
 
-const char FRAME_HEADER[] = "P6 256 256 255 ";
-const char SPACE[] = " ";
+const char FRAME_HEADER[] = "P6 640 300 255 ";
+char mode[10];
+const char color_mode[] = "color";
+const char mono_mode[] = "mono";
+char video_number;
+//char path_stream[] = "/media/developer/SDHC/VIDEO- .BIN";
 
 //ЗАПОЛНЯЕТ ФАЙЛ СГЕНЕРИРОВАННЫМИ ВИДЕОДАННЫМИ
 //ПАРАМЕТР	* path		- указатель на путь к файлу, в который будут записаны видеоданные
@@ -31,7 +35,9 @@ void generateStream(const char * path)
 {
 	FILE * file = fopen(path, "wb");
 
-	uint16_t data[256][256];
+	uint16_t frame_width = 320;
+	uint16_t frame_height = 240;
+	uint16_t data[frame_width][frame_height];
 	uint32_t start_frame	= INV_START_FRAME;
 	uint32_t end_row		= INV_END_ROW;
 	uint32_t end_frame		= INV_END_FRAME;
@@ -40,11 +46,11 @@ void generateStream(const char * path)
 	for(int k = 0; k < 150; k++)
 	{
 		fwrite(&start_frame, MARKER_SIZE, 1, file);
-		for (int i = 0; i < 256; i++)
+		for (int i = 0; i < frame_width; i++)
 		{
-			for (int j = 0; j < 256; j++)
+			for (int j = 0; j < frame_height; j++)
 			{
-				data[i][j] = I + k;
+				data[i][j] = 0b0000011111100000 + k;
 				fwrite(&data[i][j], 2, 1, file);
 				I++;
 			}
@@ -82,6 +88,7 @@ int createFrame(uint16_t * stream, int length, int pointer, const char * path_fr
 {
 	FILE * file_frame  = fopen(path_frame, "wb");		//файл со сформиррованным кадром
 	int k = 0;
+	uint8_t Y, Cb, Cr;
 
 	for(int i = pointer; i < (length / 2); i++)
 	{
@@ -90,16 +97,16 @@ int createFrame(uint16_t * stream, int length, int pointer, const char * path_fr
 		{
 			i = i + 2;
 
-			uint8_t red, green, blue;
+			int16_t red, green, blue;
 			fwrite(FRAME_HEADER, strlen(FRAME_HEADER), 1, file_frame);
-			printf("Найдено начало кадра\n");
+			printf("Найдено начало кадра [пиксель номер %d]\n", i - 2);
 			for(; i < length; i++)
 			{
-				if ((	(stream[i + 0] << 16) |
+				/*if ((	(stream[i + 0] << 16) |
 						(stream[i + 1] << 0 )	) == END_ROW)
 				{
 					i = i + 2;
-				}
+				}*/
 				if ((	(stream[i + 0] << 16) |
 						(stream[i + 1] << 0 )	) == END_FRAME)
 				{
@@ -108,17 +115,57 @@ int createFrame(uint16_t * stream, int length, int pointer, const char * path_fr
 					return i + 2;
 				}
 
-				red = (uint16_t)(stream[i] >> 11);
+
+				if (!strcmp(mode, color_mode))
+				{
+					Y = (uint8_t)stream[i];
+					if (i % 2 == 0)
+					{
+						Cb = (uint8_t)(stream[i] >> 8);
+						Cr = (uint8_t)(stream[i+1] >> 8);
+					}
+
+					red = round(Y + 1.402 * (Cr - 128));
+					green = round(Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128));
+					blue = round(Y + 1.772 * (Cb - 128));
+
+					if (red > 255)	red = 255;		if (red < 0)	red = 0;
+					if (green > 255)green = 255;	if (green < 0)	green = 0;
+					if (blue > 255)	blue = 255;		if (blue < 0)	blue = 0;
+				}
+
+				else if (!strcmp(mode, mono_mode))
+				{
+					uint8_t k_temp = (uint8_t)stream[i];
+					stream[i] = stream[i] >> 8 ;
+					stream[i] = (k_temp << 8) | stream[i];
+					red = (uint8_t)(stream[i] >> 8);
+					green = (uint8_t)(stream[i] >> 8);
+					blue = (uint8_t)(stream[i] >> 8);
+				}
+				else {printf("ERROR");break;}
+
+				/*red = (uint16_t)(stream[i] >> 11);
 				red = (uint8_t)(red * 255 / 31);
 				green = (uint16_t)((uint16_t)(stream[i] << 5) >> 10);
 				green = (uint8_t)(green * 255 / 63);
 				blue = (uint16_t)((uint16_t)(stream[i] << 11) >> 11);
-				blue = (uint8_t)(blue * 255 / 31);
+				blue = (uint8_t)(blue * 255 / 31);*/
+				/*uint8_t k_temp = (uint8_t)stream[i];
+				stream[i] = stream[i] >> 8 ;
+				stream[i] = (k_temp << 8) | stream[i];
+				red = (uint8_t)(stream[i] >> 8);
+				green = (uint8_t)(stream[i] >> 8);
+				blue = (uint8_t)(stream[i] >> 8);*/
+
+				/*red = (uint8_t)(stream[i]);
+				green = (uint8_t)(stream[i]);
+				blue = (uint8_t)(stream[i]);*/
 
 				fwrite(&red, 1, 1, file_frame);
 				fwrite(&green, 1, 1, file_frame);
 				fwrite(&blue, 1, 1, file_frame);
-				k++;
+				//k++;
 			}
 
 		}
@@ -128,21 +175,44 @@ fclose(file_frame);
 return -1;
 }
 
+/*void set_mode_()
+{
+	printf("ВВЕДИТЕ РЕЖИМ ОБРАБОТКИ ВИДЕО\n");
+	scanf("%s", mode);
+
+	printf("ВВЕДИТЕ НОМЕР ВИДЕО\n");
+	scanf("%s", &video_number);
+
+	path_stream[28] = video_number;
+	printf("%s\n", path_stream);
+}*/
+
+
 
 //MAIN*****************
 int main()
 {
-	const char	path_stream[]	= "/home/developer/git/CanSat-2017/src-ground_STM32/video.bin";
+	const char path_stream[] = "/media/developer/SDHC/VIDEO-13.BIN";
 	char 		path_folder[]	= "/home/developer/Рабочий стол/Frames/frame-";
-	const char	expansion[]		= ".ppm";
+	const char	extension[]		= ".ppm";
+
+	//set_mode_();
+	printf("ВВЕДИТЕ РЕЖИМ ОБРАБОТКИ ВИДЕО\n");
+	scanf("%s", mode);
+
+		/*printf("ВВЕДИТЕ НОМЕР ВИДЕО\n");
+		scanf("%s", &video_number);
+
+		//path_stream[28] = video_number;
+		printf("%s\n", path_stream);*/
 
 	char number[4];
 	int n = 0;
-	char path_frame [strlen(path_folder) + strlen(number) + strlen(expansion) + 1];
+	char path_frame [strlen(path_folder) + strlen(number) + strlen(extension) + 1];
 
 	//СОЗДАЕМ ФАЙЛ С ВИДЕОПОТОКОМ
 	FILE * file_stream = fopen(path_stream, "r");		//файл с видеопотоком
-	generateStream(path_stream);
+	//generateStream(path_stream);
 	if (file_stream == NULL)
 	{
 		printf("ERROR");								//выводит ERROR если невозможно открыть файл
@@ -153,25 +223,24 @@ int main()
 	printf("Размер потока видео: %d байт(а)\n", pointer);
 
 
-
 	//выделяем в динамической памяти массив элементов uint16_t длины pointer и создаем указатель на него
 	uint16_t * stream_uint = (uint16_t*)malloc(pointer);
 
 	fseek(file_stream, 0, SEEK_SET);					//перемещаем указатель в начало файла
-	fread(stream_uint, 2 * pointer, 1, file_stream);	//читаем из file_stream в созданный массив stream_uint один раз pointer бит
+	fread(stream_uint, 2 * pointer-1, 1, file_stream);	//читаем из file_stream в созданный массив stream_uint один раз pointer бит
 
 	int state = 0;
 	while(1)
 	{
 		if (state == -1)
 		{
-			printf("Кадра № %d записан не полностью.\nКОНЕЦ ВИДЕОПОТОКА\n", n - 1);
+			printf("Кадр № %d записан не полностью.\nКОНЕЦ ВИДЕОПОТОКА\n", n - 1);
 			break;
 		}
 		setFrameNumber(n, number, 4);
 		strcpy(path_frame, path_folder);
 		strncat(path_frame, number, 4);
-		strcat(path_frame, expansion);
+		strcat(path_frame, extension);
 
 		printf("\n===================================\n");
 		printf("Путь к кадру № %d: ", n);
